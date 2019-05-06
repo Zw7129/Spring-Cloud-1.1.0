4dfc0fccbd96db5b2aff522fb84d6ba7b31e7c2a
pc <pc@acer-pc>
2019-05-06 14:39:52 +0800

    实现访问html

--- sc_eureka-consumer/src/main/java/com/jorian/sc_cloud/controller/HelloController.java
+++ sc_eureka-consumer/src/main/java/com/jorian/sc_cloud/controller/HelloController.java
@@ -1,21 +1,28 @@
 package com.jorian.sc_cloud.controller;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
- 
-@RestController
+
+@Controller
 public class HelloController {
 	@Autowired
 	private RestTemplate resttemplate;
-	
+
 	@RequestMapping("/hello")
-	public String hello(){
-		String url="http://provider-user/findUser";
-		
-		//返回值类型和我们的业务返回值一致
+	public String hello() {
+		String url = "http://provider-user/findUser";
+
+		// 返回值类型和我们的业务返回值一致
 		return resttemplate.getForObject(url, String.class);
+
+	}
 	
+	@RequestMapping("/index")
+	public String index(){
+		
+		return "index2";
 	}
 }
